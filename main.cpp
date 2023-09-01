077d1537d80b793e0c2600c5a52b86ca361430f1
aashiq <ashaik11@asu.edu>
2023-09-01 13:47:35 -0700

    added glm library

--- main.cpp
+++ main.cpp
@@ -188,6 +188,11 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {}
+    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {}
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
