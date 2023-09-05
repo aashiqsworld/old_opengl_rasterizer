e2051dd1d0fa5cfd90d9bb78704d4d16b2f6dc74
aashiq <ashaik11@asu.edu>
2023-09-04 17:39:47 -0700

    added camera class

--- cmake-build-debug/shader.fs
+++ cmake-build-debug/shader.fs
@@ -6,8 +6,10 @@ in vec2 TexCoord;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
+uniform vec3 lightColor;
+uniform vec3 objectColor;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColor = vec4(lightColor * texture(texture1, TexCoord).xyz * objectColor, 1.0);
 }
--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 #include "glad.h"
 #include <glfw3.h>
 #include "glad.c"
-#include "shader.h"
-#include "stb_image.h"
 #include "include/glm/glm.hpp"
 #include "include/glm/gtc/matrix_transform.hpp"
 #include "include/glm/gtc/type_ptr.hpp"
+#include "stb_image.h"
+
+#include "shader.h"
+#include "camera.h"
+
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -21,17 +24,17 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // camera variables
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
-float pitch = 0.0f, yaw = 0.0f;
-float fov = 45.0f;
-
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 // mouse variables
 float lastX = 400, lastY = 300;
 bool firstMouse = true;
+
+// timing variables
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// light variables
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main()
 {
@@ -72,7 +75,8 @@ int main()
 
 
     // link shader
-    Shader ourShader("shader.vs", "shader.fs");
+    Shader litShader("shader.vs", "shader.fs");
+    Shader lightCubeShader("lightShader.vs", "lightShader.fs");
 
     // enable depth test
     glEnable(GL_DEPTH_TEST);
@@ -80,15 +84,51 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
 
-    float vertices[] = {
-            // positions          // colors           // texture coords
-            0.5f,  0.5f, 0.0f,        1.0f, 0.0f, 0.0f,     1.0f, 1.0f,   // top right
-            0.5f, -0.5f, 0.0f,       0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-            -0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+    float boxVertices[] = {
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+
+            -0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            -0.5f, -0.5f,  0.5f,
+
+            -0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
+            -0.5f, -0.5f,  0.5f,
+            -0.5f, -0.5f, -0.5f,
+
+            -0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f, -0.5f
     };
 
-    float boxVertices[] = {
+    float boxVerticesWithTexCoords[] = {
             -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
             0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
@@ -146,7 +186,7 @@ int main()
     };
 
 
-
+    // --------- mesh vertex stuff -----------
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -154,10 +194,7 @@ int main()
     // bind the Vertex Arrays Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(boxVertices), boxVertices, GL_STATIC_DRAW);
-
-//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(boxVerticesWithTexCoords), boxVerticesWithTexCoords, GL_STATIC_DRAW);
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -166,6 +203,15 @@ int main()
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    // --------- light stuff -----------
+    unsigned int lightVAO, lightVBO;
+    glGenVertexArrays(1, &lightVAO);
+    glGenBuffers(1, &lightVBO);
+    glBindVertexArray(lightVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, lightVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(boxVertices), boxVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     // -------- texture stuff -------
     stbi_set_flip_vertically_on_load(true);
@@ -211,9 +257,9 @@ int main()
     }
     stbi_image_free(data);
 
-    ourShader.use();
-    ourShader.setInt("texture1", 0);
-    ourShader.setInt("texture2", 1);
+    litShader.use();
+    litShader.setInt("texture1", 0);
+    litShader.setInt("texture2", 1);
 
     // testing start --------------
 
@@ -225,7 +271,6 @@ int main()
     // -----------
     while (!glfwWindowShouldClose(window))
     {
-
         // frame time operations
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
@@ -248,34 +293,48 @@ int main()
         glBindTexture(GL_TEXTURE_2D, texture2);
 
         // send matrices to shader
+        litShader.use();
+        litShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+        litShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
 
-        const float radius = 10.0f;
-        float camX = sin(glfwGetTime()) * radius;
-        float camZ = cos(glfwGetTime()) * radius;
-        auto view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-//        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+        auto view = camera.GetViewMatrix();
         glm::mat4 projection;
-        projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(camera.Zoom), 800.0f / 600.0f, 0.1f, 100.0f);
 
-        ourShader.setMat4("view", view);
-        ourShader.setMat4("projection", projection);
+        litShader.setMat4("view", view);
+        litShader.setMat4("projection", projection);
 
         glBindVertexArray(VAO);
+        glm::mat4 model = glm::mat4(1.0f);
 
         for(int i = 0; i < 10; i++)
         {
-            glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i + (i % 3 == 0 ? (float)glfwGetTime() * 50 : 0);
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            ourShader.setMat4("model", model);
+
+            litShader.setMat4("model", model);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
-        // render container
-        ourShader.use();
-//        glDrawArrays(GL_TRIANGLES, 0, 36);
+        // render light
+        lightCubeShader.use();
+        glBindVertexArray(lightVAO);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f));
+        lightCubeShader.setMat4("model", model);
+        lightCubeShader.setMat4("view", view);
+        lightCubeShader.setMat4("projection", projection);
+
+        glBindVertexArray(lightVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -304,13 +363,13 @@ void processInput(GLFWwindow *window)
 
     const float cameraSpeed = 2.5f * deltaTime;
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        cameraPos += cameraSpeed * cameraFront;
+        camera.ProcessKeyboard(FORWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+        camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        cameraPos -= cameraSpeed * cameraFront;
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
 void mouse_callback(GLFWwindow* window, double xPos, double yPos)
@@ -332,31 +391,12 @@ void mouse_callback(GLFWwindow* window, double xPos, double yPos)
     xOffset *= sensitivity;
     yOffset *= sensitivity;
 
-    // Add the offset values to the camera's yaw and pitch values
-    yaw += xOffset;
-    pitch += yOffset;
-
-    // Add some constraints to the minimum/maximum pitch values
-    if(pitch > 89.0f)
-        pitch = 89.0f;
-    if(pitch < -89.0f)
-        pitch = -89.0f;
-
-    // calculate the direction vector
-    glm::vec3 direction;
-    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-    direction.y = sin(glm::radians(pitch));
-    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-    cameraFront = glm::normalize(direction);
+    camera.ProcessMouseMovement(xOffset, yOffset);
 }
 
 void scroll_callback(GLFWwindow* window, double xOffset, double yOffset)
 {
-    fov -= (float)yOffset;
-    if(fov < 1.0f)
-        fov = 1.0f;
-    if(fov > 45.0f)
-        fov = 45.0f;
+    camera.ProcessMouseScroll(yOffset);
 }
 
 
