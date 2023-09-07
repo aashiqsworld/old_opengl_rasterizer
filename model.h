64e7d2e201607077e63c1587cbff8604846ddbd4
aashiq <ashaik11@asu.edu>
2023-09-07 11:50:53 -0700

    fixed assimp header include path

--- model.h
+++ model.h
@@ -9,8 +9,11 @@
 #include "include/glm/glm.hpp"
 #include "shader.h"
 #include "mesh.h"
+#include "assimp/Importer.hpp"
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
-
+using namespace std;
 
 class Model
 {
@@ -26,8 +29,11 @@ private:
     std::vector<Mesh> meshes;
     std::string directory;
 
-    void loadModel(std::string path);
-//    void processNode(aiNode
+    void loadModel(string path);
+    void processNode(aiNode *node, const aiScene *scene);
+    Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+    vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type,
+                                         string typeName);
 
 };
 
